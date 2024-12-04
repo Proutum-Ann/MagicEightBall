b5a3d2448ebad3234780711704902d1296efc1b3
Proutum-Ann <526ALiang@frhsd.com>
2024-12-04 09:19:40 -0500

    finally got the image to work (crying)

--- script.js
+++ script.js
@@ -7,39 +7,41 @@ document.getElementById('question').innerHTML = userQuestion
 randomNumber = Math.floor(Math.random() * 8)
 
 let eightBall = ''
-// let resImg = document.getElementById('image').src
+let resImage = ''
 
-switch(randomNumber) {
-  case 1:
-    eightBall = 'It is certain';
-    break;
-  case 2:
-    eightBall = 'It is decidedly so';
-    break;
-  case 3:
-    eightBall = 'Reply hazy try again';
-    break;
-  case 4:
-    eightBall = 'Cannot predict now';
-    break;
-  case 5:
-    eightBall = 'Do not count on it';
-    break;
-  case 6:
-    eightBall = 'My sources say no';
-    break;
-  case 7:
-    eightBall = 'Outlook not so good';
-    break;
-  case 8:
-    eightBall = 'Signs point to yes';
-    break;
-  case 0:
-    eightBall = 'Do it.';
-    break;
+if (randomNumber === 0) {
+  eightBall = 'Do it.';
+  resImage = '<img src="imgs/wonderhoy.webp">';
+} else if (randomNumber === 1) {
+  eightBall = 'It is certain';
+  resImage = '<img src="imgs/wonderhoy.webp">';
+} else if (randomNumber === 2) {
+  eightBall = 'It is decidedly so';
+  resImage = '<img src="imgs/wonderhoy.webp">';
+} else if (randomNumber === 3) {
+  eightBall = 'Reply hazy try again';
+  resImage = '<img src="imgs/wonderhoy.webp">';
+} else if (randomNumber === 4) {
+  eightBall = 'Cannot predict now';
+  resImage = '<img src="imgs/wonderhoy.webp">';
+} else if (randomNumber === 5) {
+  eightBall = 'Do not count on it';
+  resImage = '<img src="imgs/wonderhoy.webp">';
+} else if (randomNumber === 6) {
+  eightBall = 'My sources say no';
+  resImage = '<img src="imgs/wonderhoy.webp">';
+} else if (randomNumber === 7) {
+  eightBall = 'Outlook not so good';
+  resImage = '<img src="imgs/ominousbell.jpg">';
+} else if (randomNumber === 8) {
+  eightBall = 'Signs point to yes';
+  resImage = '<img src="imgs/wonderhoy.webp">';
 }
-
 
 console.log(eightBall)
 
 document.getElementById('answer').innerHTML = eightBall
+
+console.log(resImage)
+
+document.getElementById('image').innerHTML = resImage
